73e207a2bdcca0f917f738223873763c21a58f76
GerardBR3270 <lugo044@gmail.com>
2024-01-05 20:20:48 -0600

    feat: Create ProductListPage.js
    Added a new Next.js page named ProductList as a directory. This page will serve as the foundation for the overall product listing. Initial implementation includes a basic structure with a placeholder message.

---
+++ src/app/ProductList/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductListPage = () => {
+  return (
+    <div>
+      <h1>Product List Page</h1>
+      {/* Product list should be added here */}
+    </div>
+  );
+};
+
+export default ProductListPage;
