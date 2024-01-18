ae58a00b1de7c1363fddfdb66dedabe4ea3e471f
GerardBR3270 <lugo044@gmail.com>
2024-01-17 20:58:44 -0600

    feat: Implement style variables and standardize common attributes
    Introduce a set of style variables to centralize design parameters such as colors, font sizes, spacing, and other common attributes. Standardizing these values enhances consistency across the project's styles, facilitating future updates and maintenance. This commit establishes a cohesive and easily maintainable styling structure.

---
+++ src/components/navbar/navBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navBar.scss';
+
+export const NavBar = () => {
+  return (
+    <div className='navBar'>
+      <p>Opción 1</p>
+      <p>Opción 2</p>
+      <p>Opción 3</p>
+      <p>Opción 4</p>
+      <p>Opción 5</p>
+    </div>
+  );
+};
